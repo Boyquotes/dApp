ebc65e38c2a43e21cd51dabff0cea1330e116b93
Boyquotes <webmaster@linux-live-cd.org>
2021-12-30 20:13:28 +0100

    Updates

---
+++ precache-manifest.c42afcf65ac9ed18619775c81cc5d286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad0c937e417f174f924663aac587d9d",
+    "url": "/dApp/index.html"
+  },
+  {
+    "revision": "41b7bd46a0e4ec7b5a47",
+    "url": "/dApp/static/css/main.9b0a0b89.chunk.css"
+  },
+  {
+    "revision": "a8bdf43fcf828dc60fae",
+    "url": "/dApp/static/js/2.7b508fb0.chunk.js"
+  },
+  {
+    "revision": "41b7bd46a0e4ec7b5a47",
+    "url": "/dApp/static/js/main.434b435c.chunk.js"
+  },
+  {
+    "revision": "99398862958f96e43d28",
+    "url": "/dApp/static/js/runtime-main.a2048d26.js"
+  },
+  {
+    "revision": "7dfe335312500f6024eab110d7c69c5c",
+    "url": "/dApp/static/media/tree333-1.7dfe3353.png"
+  }
+]);
